f24892f23d667d146ae95e30c9c8923f52bd6e7a
Jatin <Jchandwaney@gmail.com>
2020-12-27 12:04:09 +0800

    Revert "Merge branch 'master' into origin/develop-Jatin"
    This reverts commit 054710ca8348e4d152d4c59b1e5422936067a6cc.

--- routes/restaurants.js
+++ routes/restaurants.js
@@ -2,16 +2,15 @@ const router = require('express').Router();
 const ObjectID = require('mongodb').ObjectID;
 const formidable = require('formidable');
 const fs = require('fs');
-
-//home page redirect to the first page of restaurants
+const url = require('url');
+const assert = require('assert');
+//get all the restaurants in the collection
+//redirect into the first page of all restaurants
+//TODO: just make a first page for this system
 router.get('/', (req, res, next) => {res.redirect('/restaurants/1');});
 
-//create new restaurant page
-router.get('/restaurants/create', (req, res, next) => { 
-    res.render('createrestaurant');
-});
-
-//show all restaurants using pagination
+router.get('/creatrestaurant', (re, res, next) => { 
+    res.render('creatrestaurant');});
 router.get('/restaurants/:page', async (req, res, next)=>{
     let page = req.params.page;
     let pageLimit = 25;
@@ -30,7 +29,6 @@ router.get('/restaurants/:page', async (req, res, next)=>{
     }
 });
 
-//look at the individual restaurant
 router.get('/restaurant/:id', (req, res, next)=>{
     let searchCrit = {
         _id: new ObjectID(req.params.id)
@@ -77,53 +75,64 @@ router.get('/api/restaurant/cuisine/:cuisine', (req, res, next)=>{
     });
 });
 
-//search system
-router.get('/search/:page', async (req, res, next)=>{
-    let searchQuery = {};
-    searchQuery[req.query.filter] = req.query.query
-    let page = req.params.page;
-    let pageLimit = 25;
-    let documents = await Promise.all([global.db.collection('restaurants').countDocuments(searchQuery)]);
-    let pageCount = Math.ceil(documents[0] / pageLimit);
-    if(page==1){
-        //something happens here
-        global.db.collection('restaurants').find(searchQuery).limit(pageLimit).toArray((err, results)=>{
-            if(err) console.log(err);
-            res.render('restaurants', {restaurants:results, currentPage: page, pageCount, search: true, filter: req.query.filter, query: req.query.query});
-        });
-    }else{
-        global.db.collection('restaurants').find(searchQuery).skip(page*pageLimit).limit(pageLimit).toArray((err, results)=>{
-            if(err) console.log(err);
-            res.render('restaurants', {restaurants:results, currentPage:page, pageCount, search: true, filter: req.query.filter, query: req.query.query});
-        })
-    }
-});
-
 //rate restaurants, only one review per user per restaurant
 router.get('/restaurant/:resid/rate', (req, res, next)=>{
-    let searchCrit = {
-        _id: new ObjectID(req.params.resid)
-    }
-
-    global.db.collection('restaurants').find(searchCrit).toArray((err, result)=>{
-        if(err){
-            console.log(err);
-        }else{
-            let userFound = false;
-            for(let i = 0; i<result[0].grades.length; i++){
-                if(result[0].grades[i].username == req.session.username){
-                    userFound = true;
-                    res.redirect(`/restaurant/${req.params.resid}`);
-                }
-            }
-            //if you haven't rated the restaurant, then you can rate
-            if(userFound == false){
-                res.render('rate', {resid:req.params.resid});
-            }
-        }
-    })
+    res.render('rate', {resid:req.params.resid});
 });
-
+//delete restaurant
+router.get('/restaurant/:resid/delete', (req, res, next)=>{
+    let username=req.session.username;
+    let owner= global.db.collection('restaurants').find({resid:req.params.resid});
+    if (owner.username == username){
+        global.db.collection('restaurants').remove({resid:req.params.resid})
+    }
+    else{
+    res.render('warn');
+    }
+});
+//update restaurant
+router.get('/restaurant/:resid/edit', (req, res, next)=>{
+    let username=req.session.username;
+    let owner= global.db.collection('restaurants').find({resid:req.params.resid});
+    if (owner.username == username){
+        res.render('edit', {resid:req.params.resid});
+    }
+    else{
+    res.render('warn');
+    }
+});
+router.post('/api/updaterestaurant', (req, res, next)=>{
+    let restaurantname = req.body.name;
+    let borough = req.body.borough;
+    let cuisine = req.body.cuisine;
+    let street = req.body.street;
+    let building = req.body.building;
+    let zipcode = req.body.zipcode;
+    let coord = req.body.coord;
+    let score = req.body.score;
+    let owner = req.session.username;
+    var changeDoc = {
+        name : restaurantname,
+        borough : borough,
+        cuisine : cuisine,
+        street : street,
+        building : building,
+        zipcode : zipcode,
+        coord : coord,
+        score : score,
+        owner : owner,
+    };
+    global.db.collection('restaurants').updateOne({_id: new ObjectID(req.params.resid)},{$push: {grades: changeDoc}});
+    res.redirect(`/restaurant/${req.params.resid}`)
+    
+});
+/*
+algorithm:
+check if the user already has a review in the system for this restaurant
+if does have, cannot give a rating on this restaurant, return them back to the restaurant page with message or something
+if doesn't have, take in the data and create the review by appending it to the end of the grades array of the document.
+*/
+//TODO: TEST THIS, UNTESTED
 router.post('/api/restaurant/:resid/rate', (req, res, next)=>{
     let searchCrit = {
         _id: new ObjectID(req.params.resid)
@@ -148,176 +157,53 @@ router.post('/api/restaurant/:resid/rate', (req, res, next)=>{
     });
 });
 
-//delete restaurant
-router.post('/api/restaurant/:resid/delete', (req, res, next)=>{
-    let searchCrit = {
-        _id: new ObjectID(req.params.resid)
-    };
-    global.db.collection('restaurants').find(searchCrit).toArray((err, result)=>{
-        if (err) console.log(error);
-        if(result[0].owner){
-            if (result[0].owner == req.session.username){
-                global.db.collection('restaurants').deleteOne(searchCrit)
-                .then(result=>{
-                    if(result){
-                        res.redirect('/restaurants/1')
-                    }
-                })
-                .catch(err=>{
-                    if(err){
-                        res.redirect(`/restaurant/${req.params.resid}`, {message: 'error deleting file'});
-                    }
-                });
-            }
-            else{
-                res.render('warn');
-            }
-        }else{
-            //delete it if there is no owner
-            global.db.collection('restaurants').deleteOne(searchCrit)
-            .then(result=>{
-                if(result){
-                    res.redirect('/restaurants/1')
-                }
-            })
-            .catch(err=>{
-                if(err){
-                    res.redirect(`/restaurant/${req.params.resid}`, {message: 'error deleting file'});
-                }
-            });
-        }
-    })
+//search system
+router.get('/search/:page', async (req, res, next)=>{
+    let searchQuery = {};
+    searchQuery[req.query.filter] = req.query.query
+    let page = req.params.page;
+    let pageLimit = 25;
+    let documents = await Promise.all([global.db.collection('restaurants').countDocuments(searchQuery)]);
+    let pageCount = Math.ceil(documents[0] / pageLimit);
+    if(page==1){
+        //something happens here
+        global.db.collection('restaurants').find(searchQuery).limit(pageLimit).toArray((err, results)=>{
+            if(err) console.log(err);
+            res.render('restaurants', {restaurants:results, currentPage: page, pageCount, search: true, filter: req.query.filter, query: req.query.query});
+        });
+    }else{
+        global.db.collection('restaurants').find(searchQuery).skip(page*pageLimit).limit(pageLimit).toArray((err, results)=>{
+            if(err) console.log(err);
+            res.render('restaurants', {restaurants:results, currentPage:page, pageCount, search: true, filter: req.query.filter, query: req.query.query});
+        })
+    }
 });
-
-//update restaurant, add more information to just make this easier lol
-router.get('/restaurant/:resid/edit', (req, res, next)=>{
-    let searchCrit = {
-        _id: new ObjectID(req.params.resid)
-    };
-    global.db.collection('restaurants').find(searchCrit).toArray((err, result)=>{
-        if (err) console.log(error);
-        if(result[0].owner){
-            if (result[0].owner == req.session.username){
-                res.render('edit', {restaurant:result[0]});
-            }
-            else{
-                res.render('warn');
-            }
-        }else{
-            res.render('edit', {restaurant:result[0]});
-        }
-    });
-});
-
-router.post('/api/restaurant/:resid/edit', (req, res, next)=>{
-    //add image insertion in here as well
-    const form = new formidable.IncomingForm();
-    form.parse(req, function(err, fields, files){
-        if(err){
-            console.log(err);
-        }else{
-            let updateQuery = {
-                name: fields.name,
-                borough: fields.borough,
-                cuisine: fields.cuisine,
-                address:{
-                    street: fields.street,
-                    building: fields.building,
-                    zipcode: fields.zipcode,
-                    coord: [fields.long,fields.lat]
-                },
-            }
-        
-            if(files.photo.size > 0){
-                fs.readFile(files.photo.path, (err, data)=>{
-                    if(err){
-                        console.log(err);
-                    }else{
-                        // convert the picture to base 64 for insertion
-                        updateQuery['mimetype'] = files.photo.type;
-                        updateQuery['photo'] = new Buffer.from(data).toString('base64');
-                        // insertion of the new document
-                        // find the restaurant we want to update
-                        let findQuery = {
-                            _id: new ObjectID(req.params.resid),
-                        }
-                        global.db.collection('restaurants').updateOne(findQuery, {$set: updateQuery}, {upsert:true})
-                        .then(result=>{
-                            if(result){
-                                res.redirect(`/restaurant/${req.params.resid}`);
-                            }
-                        })
-                        .catch(err=>{
-                            if(err){
-                                res.redirect(`/restaurant/${req.params.resid}`, {message: err});
-                            }   
-                        });
-                    }
-                })
-            }else{
-                //update document without the picture
-                let findQuery = {
-                    _id: new ObjectID(req.params.resid),
-                }
-                global.db.collection('restaurants').updateOne(findQuery, {$set: updateQuery}, {upsert:true})
-                .then(result=>{
-                    if(result){
-                        res.redirect(`/restaurant/${req.params.resid}`);
-                    }
-                })
-                .catch(err=>{
-                    if(err){
-                        res.redirect(`/restaurant/${req.params.resid}`, {message: err});
-                    }   
-                });
-            }
-        }
-    })
-});
-
-router.post('/api/restaurant/create', (req, res, next)=>{
-    //convert photo to base64
-    let form = new formidable.IncomingForm();
-    form.parse(req, (err, fields, files)=>{
-        if(err){
-            console.log(err);
-        }else{
-            let insertQuery = {
-                name: fields.name || '',
-                borough: fields.borough || '',
-                cuisine: fields.cuisine || '',
-                address: {
-                    street: fields.street || '',
-                    building: fields.building || '',
-                    zipcode: fields.zipcode || '',
-                    coord: [fields.lat, fields.long] || [],
-                },
-                grades: [],
-                owner: req.session.username
-            }
-            if(files.photo.size > 0){
-                fs.readFile(files.photo.path, (err, data)=>{
-                    if(err){
-                        console.log(err);
-                    }else{
-                        //convert the picture to base64
-                        insertQuery['mimetype'] = files.photo.type;
-                        insertQuery['photo'] = new Buffer.from(data).toString('base64');
-                        //insertion here
-                        global.db.collection('restaurants').insertOne(insertQuery)
-                        .then(result=>{
-                            res.redirect('/');
-                        })
-                    }
-                });
-            }else{
-                global.db.collection('restaurants').insertOne(insertQuery)
-                .then(result=>{
-                    res.redirect('/');
-                })
-            }
-        }
-    });   
-});
-
+router.post('/api/creatrestaurant', (req, res, next)=>{
+            let restaurantname = req.body.name;
+            let borough = req.body.borough;
+            let cuisine = req.body.cuisine;
+            let street = req.body.street;
+            let building = req.body.building;
+            let zipcode = req.body.zipcode;
+            let coord = req.body.coord;
+            let score = req.body.score;
+            let owner = req.session.username;
+            var newrestaurant = {
+                name : restaurantname,
+                borough : borough,
+                cuisine : cuisine,
+                street : street,
+                building : building,
+                zipcode : zipcode,
+                coord : coord,
+                score : score,
+                owner : owner,
+            };
+                    global.db.collection('restaurants').insertOne(newrestaurant, (err)=>{
+                        if(err) console.log(err);
+                        res.redirect('/');
+                    })
+              
+            
+        });
 module.exports.router = router;
